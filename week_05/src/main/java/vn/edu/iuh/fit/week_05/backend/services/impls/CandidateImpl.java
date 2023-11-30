230cbd88c2cda070dc89ec8a83f6041202e29c3a
trungthinh2k2 <89030667+trungthinh2k2@users.noreply.github.com>
2023-11-30 17:56:59 +0700

    update

--- week_05/src/main/java/vn/edu/iuh/fit/week_05/backend/services/impls/CandidateImpl.java
+++
@@ -1,7 +0,0 @@
-package vn.edu.iuh.fit.week_05.backend.services.impls;
-
-import vn.edu.iuh.fit.week_05.backend.services.CandidateService;
-
-
-public class CandidateImpl implements CandidateService {
-}
